b59a8d59a723ee794926f4e6e8532c15b025895c
alexandre.deucher <alexandredeucher@gmail.com>
2026-09-22 10:17:36 -0300

    fix: :bug: correcting go syntax

--- math_test.go
+++ math_test.go
@@ -2,9 +2,10 @@ package main
 
 import "testing"
 
-func testSum(t *testing.T) {
+func TestSum(t *testing.T) {
 	total := sum(20, 20)
 
 	if total != 40 {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 40)
+	}
 }
